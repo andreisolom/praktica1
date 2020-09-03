b1face6df8854da472a0c84c79e722c602a584d3
ivc6-10 <dada@gmail.com>
2020-09-03 11:09:47 +0300

    файл

---
+++ src/NewJava.java
@@ -0,0 +1,26 @@
+public class NewJava {
+    public static void main(String[] orgs) {
+        int a = 3;
+        int k = 0;
+        int c = 0;
+        int u = 0;
+        int n = 0;
+        int m = 0;
+        int[] arr = new int[10];
+        int[] arr1 = {10, 3, 13};
+        int b = arr1[2];
+        for (int i = 0; i < a; i++) {
+            c = c + arr1[i];
+        }
+        System.out.println(
+                c);
+
+        while (k < a) {
+            u = u + arr1[k];
+            k = k + 1;
+        }
+        System.out.println(u);
+
+    }
+}
+
